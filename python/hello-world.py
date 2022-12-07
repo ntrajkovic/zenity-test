b0d991311d9b0edb3c10dbb591fa746835fa97d3
Nenad Trajkovic <nenad.trajkovic@gmail.com>
2022-12-07 15:46:58 +0100

    Add small improvements

--- python/hello-world.py
+++ python/hello-world.py
@@ -10,8 +10,16 @@
 import subprocess
 
 if __name__ == "__main__":
+
+    print("\n",
+          "Zenity Test\n",
+          "Test Info dialog\n", sep = "")
+
     subprocess.run(["zenity",
         "--info",
         "--width=200",
         "--title=Zenity Test",
-        "--text=Hello World!"])
+        "--text=Hello World!"],
+        stderr = subprocess.PIPE, text = True)
+
+    print("https://github.com/ntrajkovic/zenity-test\n")
